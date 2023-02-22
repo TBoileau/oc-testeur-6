e5f7a1a1a529ab179445e865384350548490abf7
Thomas Boileau (toham) <t-boileau@email.com>
2023-02-22 11:36:08 +0100

    Carousel (#9)
    * feat: ajout des flèches (#3)
    
    * feat: ajout d'écouteur au clic sur les flèches (#4)
    
    * feat: ajout bullet points (#5)
    
    * feat: slide des images (#6,#7)

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,32 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+document.querySelector('.arrow_left').addEventListener('click', () => {
+  renderSlide(currentSlide.previous);
+});
+
+document.querySelector('.arrow_right').addEventListener('click', () => {
+	renderSlide(currentSlide.next);
+});
+
+slides.forEach((slide, index) => {
+    slide.dot = document.createElement('span');
+    slide.dot.classList.add('dot');
+    document.querySelector('.dots').appendChild(slide.dot);
+		slide.next = slides[index + 1] || slides[0];
+		slide.previous = slides[index - 1] || slides[slides.length - 1];
+});
+
+let currentSlide = slides[0];
+
+const renderSlide = (slide) => {
+	currentSlide.dot.classList.remove('dot_selected');
+	currentSlide = slide;
+	currentSlide.dot.classList.add('dot_selected');
+	document.querySelector('#banner .banner-img').src = `./assets/images/slideshow/${currentSlide.image}`;
+	document.querySelector('#banner p').innerHTML = currentSlide.tagLine;
+}
+
+renderSlide(currentSlide);
